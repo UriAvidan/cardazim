b5abb6f92c9c958393d793822057b993b50cd32d
UriAvidan <aavidan.uri@gmail.com>
2026-09-16 16:47:51 +0300

    formatting

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import struct
 import threading
 import time
-import connection
+import Connection
 import listener
 ###########################################################
 ####################### YOUR CODE #########################
@@ -22,45 +22,12 @@ def run_server(ip, port):
             t = threading.Thread(target=handle_client, args=(conn, addr))
             t.start()
 
-    # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server.bind((ip, port))
-    # server.listen()
-    # connections = {}
-    # threads = []
-
-    # while True:
-    #     conn, addr = server.accept()
-    #     if not addr in connections.items():
-    #         connections[conn] = addr
-    #         t = threading.Thread(target=handle_client, args=(conn, addr))
-    #         threads.append(t)
-    #         t.start()
 
 
 def handle_client(conn, addr):
-    with connection.Connection(conn) as c:
+    with Connection.Connection(conn) as c:
         print(c.recieve_message())
 
-    # message = ""
-
-    # while True:
-    #     packet_message = ""
-    #     data = conn.recv(4096)
-    #     if len(data) == 0:
-    #         break
-    #     num = struct.unpack("<I", data[:4])[0]
-    #     packet_message += data[4:].decode("utf-8")
-    #     while num < len(packet_message):
-    #         data = conn.recv(4096)
-    #         if len(data) == 0:
-    #             break
-    #         packet_message += data.decode("utf-8")
-    #     if packet_message:
-    #         message += packet_message
-    # print(f"From client: {message}")
-    # # time.sleep(5)
-    # conn.close()
-    # print("client disconnected")
 
 
 ###########################################################
