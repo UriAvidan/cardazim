dcc5428948f5549297440537ca60fbe4efc972ce
UriAvidan <aavidan.uri@gmail.com>
2026-09-16 17:44:49 +0300

    added __repr__

--- listener.py
+++ listener.py
@@ -1,9 +1,11 @@
 import socket
 
-class listener:
+
+class Listener:
     def __init__(self, host, port, backlog=1000):
         self.seeker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.seeker.bind((host,port))
+        self.seeker.bind((host, port))
+        self.backlog = backlog
 
     def start(self):
         self.seeker.listen()
@@ -13,6 +15,11 @@ class listener:
 
     def accept(self):
         return self.seeker.accept()
+
+    def __repr__(self):
+        print(
+            f"Listener (port={self.seeker.getpeername()[1]}, host={self.seeker.getpeername()[0]}, backlog={self.backlog})"
+        )
 
     def __enter__(self):
         self.start()
